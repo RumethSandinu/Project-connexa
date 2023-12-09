53d0f50a37ac0897d748685ad17b01f815dfea09
RumethSandinu <sandinu413@gmail.com>
2023-12-09 14:56:57 +0530

    optimized model
    optimized model using 2 year data range to predict the sold amount per kg of a particular item with respect to the price in RMB per kg

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import tensorflow as tf
 
-tf.saved_model.load('../Rumeth/sold_amount_model')
+tf.saved_model.load('../Rumeth/final_sold_amount_model')
 
 app = Flask(__name__)
 print(app)
